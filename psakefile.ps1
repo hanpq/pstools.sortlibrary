d5ad50fd86a7546f3f52527c227c3b3db78f6da4
Hannes Palmquist <33247576+hanpq@users.noreply.github.com>
2021-03-25 16:34:24 +0100

    Updated psakefile for module pstools.sortlibrary

--- psakefile.ps1
+++ psakefile.ps1
@@ -439,7 +439,8 @@ Task -name 'Build' -depends @(
     'PreExportClean',
     'ExportCreate',
     'ExportPushModule',
-    'PostExportClean'
+    'PostExportClean',
+    'PublishToProGet',
     'CommitAndPushRepository'
 )
 
@@ -550,6 +551,29 @@ Task -name 'PublishToGallery' -precondition { $buildconfig.PSGallery } -action {
         }
     }
 } 
+
+Task -name 'PublishToProGet' -action {
+    try
+    {
+        $Measure = Measure-Command -Expression {
+            $ExportFolder = Join-Path -Path $path_root -ChildPath ('\stage\{0}' -f $modulename) -ErrorAction Stop # "\PSSolutionModules\<ModuleName>\Export\<ModuleName>"
+            Publish-Module -Path $ExportFolder -Repository 'pstools' -NuGetApiKey $Env:PROGET_KEY -ErrorAction Stop
+        }
+        Write-CheckListItem -Message 'Published module to pstools' -Severity Positive -Milliseconds $Measure.TotalMilliseconds
+    }
+    catch
+    {
+        if ($_.exception.message -like 'The module*with version*cannot be published as the current version*is already available in the repository*')
+        {
+            Write-CheckListItem -Message 'This version is already published to pstools, please build a new version and try again...' -Severity Intermediate
+        }
+        else
+        {
+            Write-CheckListItem -Message 'Failed to publish module to pstools' -Severity Negative
+            Write-Host $_
+        }
+    }
+}
 
 Task -name 'ChangeLog' -action {
     Write-Host
@@ -802,7 +826,7 @@ Task -name 'UpdateFileList' -action {
             Push-Location -Path $path_root_source
             $AllSourceFiles = Get-ChildItem -Path $path_root_source -Exclude 'logs', 'output', 'temp' | Get-ChildItem -File -Recurse
             $AllSourceFiles | ForEach-Object {
-                $PSItem | Add-Member -MemberType NoteProperty -Name RelativePath -Value (
+                $PSItem | Add-Member -MemberType NoteProperty -name RelativePath -Value (
                     Resolve-Path -Path $PSItem.FullName -Relative
                 )
             }
@@ -888,10 +912,10 @@ Task -name 'CreateModuleHelpFiles' -action {
     try
     {
         $Measure = Measure-Command -Expression {
-            Import-Module -Name $path_modulemanifest -Scope Global -ErrorAction Stop
+            Import-Module -name $path_modulemanifest -Scope Global -ErrorAction Stop
             $null = New-MarkdownHelp -Module $modulename -OutputFolder (Join-Path -Path $path_root_source -ChildPath '\en-US') -Force -ErrorAction Stop
             $null = New-ExternalHelp -Path (Join-Path -Path $path_root_source -ChildPath '\en-US') -OutputPath (Join-Path -Path $path_root_source -ChildPath '\en-US') -Force -ErrorAction Stop
-            Remove-Module -Name $modulename -Force -ErrorAction Stop
+            Remove-Module -name $modulename -Force -ErrorAction Stop
         }
     }
     catch
@@ -957,7 +981,7 @@ Task -name 'ExportSign' -precondition { $buildconfig.Sign } -action {
         {
             try
             {
-                $Cert = New-CodeSigningCert -Name 'HannesPalmquist' -FriendlyName 'HannesPalmquist' -ErrorAction Stop
+                $Cert = New-CodeSigningCert -name 'HannesPalmquist' -FriendlyName 'HannesPalmquist' -ErrorAction Stop
                 Write-CheckListItem -Message 'Successfully created Code Signing Certificate' -Severity Positive
             }
             catch
@@ -971,7 +995,7 @@ Task -name 'ExportSign' -precondition { $buildconfig.Sign } -action {
             Remove-Item -Path ('Cert:\CurrentUser\My\{0}' -f $Cert.Thumbprint) -Force
             try
             {
-                $Cert = New-CodeSigningCert -Name 'HannesPalmquist' -FriendlyName 'HannesPalmquist' -ErrorAction Stop
+                $Cert = New-CodeSigningCert -name 'HannesPalmquist' -FriendlyName 'HannesPalmquist' -ErrorAction Stop
                 Write-CheckListItem -Message 'Successfully renewed Code Signing Certificate' -Severity Positive
             }
             catch
