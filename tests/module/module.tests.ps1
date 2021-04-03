33c1a08d7c7b3167fdc8748b3e2c568aff9a3fad
Hannes Palmquist <33247576+hanpq@users.noreply.github.com>
2021-04-03 20:47:31 +0200

    Updated module.tests.ps1 for module pstools.sortlibrary

--- tests/module/module.tests.ps1
+++ tests/module/module.tests.ps1
@@ -40,7 +40,8 @@ BeforeAll {
                 {
                     $RawCode = Get-Content $ScriptFilePath -Raw -ErrorAction Stop -Encoding $Encoding
                     Write-Verbose -Message 'Successfully imported file'
-                } catch
+                }
+                catch
                 {
                     Write-Error -Message 'Failed to import file' -ErrorRecord $_
                     break
@@ -157,110 +158,6 @@ BeforeAll {
             }
         }
     }
-
-    function Get-PSScriptInfo
-    {
-        <#
-      .DESCRIPTION
-        Collect and parse psscriptinfo from file
-      .PARAMETER FilePath
-        Defines the path to the file from which to get psscriptinfo from.
-      .EXAMPLE
-        Get-PSScriptInfo -FilePath C:\temp\file.ps1
-        Description of example
-    #>
-
-        [CmdletBinding()] # Enabled advanced function support
-        param(
-            [ValidateScript( { Test-Path -Path $_ -PathType Leaf })][Parameter(Mandatory)][string]$FilePath
-        )
-
-        PROCESS
-        {
-            try
-            {
-                $PSScriptInfo = [ordered]@{ }
-                New-Variable astTokens -Force
-                New-Variable astErr -Force
-                $null = [System.Management.Automation.Language.Parser]::ParseFile($FilePath, [ref]$astTokens, [ref]$astErr)
-                $FileContent = $astTokens.where{ $_.kind -eq 'comment' -and $_.text.Replace("`r", '').Split("`n")[0] -like '<#PSScriptInfo*' } | Select-Object -expand text
-                $FileContent = $FileContent.Replace("`r", '').Split("`n")
-                if ($FileContent)
-                {
-                    $FileContent | Select-Object -Skip 1 | ForEach-Object {
-                        $CurrentRow = $PSItem
-                        if ($CurrentRow.Trim() -like '.*')
-                        {
-                            # New attribute found, extract attribute name
-                            $Attribute = $CurrentRow.Split('.')[1].Split(' ')[0]
-
-                            # Check if row has value
-                            if ($CurrentRow.Trim().Replace($Attribute, '').TrimStart('.').Trim().Length -gt 0)
-                            {
-
-                                # Value on same row
-                                $Value = $CurrentRow.Trim().Split(' ', 2)[1].Trim()
-
-                                # String Arrays
-                                if (@() -contains $Attribute)
-                                {
-                                    $Value = $Value.Split(',').Trim()
-                                }
-                                # Datetime
-                                if (@('CREATEDDATE' -contains $Attribute))
-                                {
-                                    $Value = $Value -as [datetime]
-                                }
-                                # System version
-                                if (@('VERSION' -contains $Attribute))
-                                {
-                                    $Value = $Value -as [system.version]
-                                }
-                                # guid
-                                if (@('GUID' -contains $Attribute))
-                                {
-                                    $Value = $Value -as [guid]
-                                }
-
-                                if (@('UNITTEST' -contains $Attribute))
-                                {
-                                    if ($Value -eq 'false')
-                                    {
-                                        $Value = $false
-                                    } elseif ($Value -eq 'true')
-                                    {
-                                        $Value = $true
-                                    } else
-                                    {
-                                        $Value = $null
-                                    }
-                                }
-
-                                # Add attribute and value to PSScriptInfo
-                                $null = $PSScriptInfo.Add($Attribute, $Value)
-                            } else
-                            {
-                                # If no value is provided populate PSScriptInfo with attribute and an empty collection as value
-                                $null = $PSScriptInfo.Add($Attribute, [collections.arraylist]::New())
-                            }
-                        } elseif ($CurrentRow -notlike '*#>*')
-                        {
-                            # If row is not an attribute and PSScriptInfo is not terminated add row as value in attribute collection
-                            $null = $PSScriptInfo.$Attribute.Add($CurrentRow.Trim())
-                        }
-
-                    }
-                    Write-Output $PSScriptInfo 
-                } else
-                {
-                    Write-Error -Message 'No valid PSScriptInfo was found in file'
-                }
-            } catch
-            {
-                Write-Error -Message 'No valid PSScriptInfo was found in file' -ErrorRecord $_
-            }
-        }
-    }   
     
     function Split-String
     {
@@ -333,7 +230,8 @@ BeforeAll {
                             $Result = $CurrentString.Split($Separator)
                             $Result | Select-Object -First $Count
                             ($Result | Select-Object -Skip $Count) -join $Separator
-                        } else
+                        }
+                        else
                         {
                             $CurrentString.Split($Separator)
                         }
